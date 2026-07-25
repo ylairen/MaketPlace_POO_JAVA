b9d768e388ff2b4968ce5686c8075d8316c05523
ylairen <ysabele.lorena12@gmail.com>
2026-07-25 10:38:16 -0300

    FreteService atualizada

--- src/service/FreteService.java
+++ src/service/FreteService.java
@@ -1,5 +1,16 @@
 package service;
 
+import model.Cidade;
+
 public class FreteService {
 
+	public double calcularFrete(Cidade origem,Cidade destino,boolean retirada) {
+		if(retirada || (origem==destino))
+			return 0.0;
+			final boolean envolveBarbalhaJuazeiro = (origem == Cidade.BARBALHA && destino == Cidade.JUAZEIRO_DO_NORTE)
+					|| (origem == Cidade.JUAZEIRO_DO_NORTE && destino == Cidade.BARBALHA);
+			if(envolveBarbalhaJuazeiro) 
+				return 15.0;
+			return 20.0;
+			}
 }
